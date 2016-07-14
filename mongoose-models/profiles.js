dfae19924edf94d822eace76420bd1ec818f03b2
dev0l <marcus.mobark@gmail.com>
2016-07-14 11:08:52 +0200

    some minor changes

--- mongoose-models/profiles.js
+++ mongoose-models/profiles.js
@@ -1,22 +1,22 @@
-module.exports = function(mongoose){
+module.exports = function (mongoose) {
 
   // Create a new mongoose schema
   var ProfilesSchema = mongoose.Schema({
-    firstName: {type: String, required: true},
-    lastName: {type: String, required: true},
-    userName: {type: String, required: true},
-    password: {type: String, required: true},
-    email: {type: String, required: true},
-    phone: {type: Number, required: false},
-    avatar: {type: String, required: false},
-    pic: {type: String, required: false},
-    date: {type: Number, required: false},
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    userName: { type: String, required: true },
+    password: { type: String, required: true },
+    email: { type: String, required: true },
+    phone: { type: Number, required: false },
+    dob: { type: Number, required: false },
+    avatar: { type: String, required: false },
+    pic: { type: String, required: false },
 
     // a relation
     // doneBy: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
   });
 
-  ProfilesSchema.pre('update', function(next) {
+  ProfilesSchema.pre('update', function (next) {
     // tell mongoose to run validators before every "update"
     // on this schema
     this.options.runValidators = true;
